5c3c044e85128b86e4b091ec42ca7716ac6e4e4a
Aishwarya R <aishuradhakrishnan21@gmail.com>
2016-10-18 19:22:30 +0530

    inintial commit

---
+++ warmup1/diff22.r
@@ -0,0 +1,9 @@
+diff22 <- function(n){
+  if (n <= 21){
+    return(21 - n)
+  } else{
+    return((n - 21) * 2) 
+  }
+    }
+
+
---
+++ warmup1/makes10.r
@@ -0,0 +1,9 @@
+makes10 <- function(a, b){
+  if (a == 10 | b == 10 | a + b == 10){
+    return(TRUE)
+  } else {
+    return(FALSE)
+  }
+}
+
+
